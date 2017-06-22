768317190c76afed756c08023dfff1ffab306b59
laineus <info@laineus.com>
2017-06-22 19:24:18 +0900

    sqlファイル修正 envファイルのサンプル追加

--- sql/db.sql
+++ sql/db.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `digimons` (
   `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(255) NOT NULL,
-  `data` text NOT NULL,
+  `data` json NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL
